44e908879e0230d94b88e75160bee0873d176e8c
Saumit Dinesan <79687674+Saumit-D@users.noreply.github.com>
2021-11-05 12:36:17 +0530

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -16,8 +16,8 @@ static char *font = "UbuntuMono Nerd Font:pixelsize=22:antialias=true:autohint=t
 
 /* Spare fonts */
 static char *font2[] = {
-		"JoyPixels:pixelsize=22:antialias=true:autohint=true",
-		"Spoqa Han Sans Neo:pixelsize=18:antialias=true:autohint=true"};
+		"JoyPixels:pixelsize=16:antialias=true:autohint=true",
+		"Spoqa Han Sans Neo:pixelsize=16:antialias=true:autohint=true"};
 //"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true",
 //"Symbola:pixelsize=10:antialias=true:autohint=true"};
 
@@ -499,3 +499,4 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
+
